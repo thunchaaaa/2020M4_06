5c661fdfdb3e447409de8ef5b02764c5d458b022
thunchaaaaa <thunchanok_du@cmu.ac.th>
2021-06-05 18:05:29 +0700

    commit(3)
    commit(3)

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-var newText = 'new Text'
-document.getElementById('ok').innerHTML = newText
+document.getElementById('ok').addEventListener('click', function (e) {
+    alert('ok is ok ')
+    console.log(e)
+}
+)
+document.getElementById('cancel').addEventListener('click', function (e) {
+    alert('ok is cancel ')
+    console.log(e)
+}
+)
+document.getElementById('no').addEventListener('click', function (e) {
+    alert('ok is no ')
+    console.log(e)
+}
+)
+document.getElementById('cancel').addEventListener('mousemove', function (c) {
+    var x = c.client;
+    var y = c.client;
+    console.log('x:' + x + ',' + 'y:' + y)
+}
+)
 
-var newText = 'nuk'
-document.getElementById('no').innerHTML = newText
+
+document.getElementById('ok').addEventListener('mousemove', function (c) {
+    var x = c.client;
+    var y = c.client;
+    console.log('x:' + x + ',' + 'y:' + y)
+}
+)
+
+document.getElementById('no').addEventListener('mousemove', function (c) {
+    var x = c.client;
+    var y = c.client;
+    console.log('x:' + x + ',' + 'y:' + y)
+}
+)
+Aa
+
